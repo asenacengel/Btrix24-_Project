e19e9e249a74d7f0c2db0928b2d50756c2aba7bb
Asena Cengel <asena.eroglu@gmail.com>
2020-04-16 00:39:14 -0400

    UnCommented code is fixed.

--- src/automation/java/tests/AbstractTestBase.java
+++ src/automation/java/tests/AbstractTestBase.java
@@ -62,21 +62,20 @@ public abstract class AbstractTestBase {
 
     @AfterMethod
     public void teardown(ITestResult testResult) {
-//        if (testResult.getStatus() == ITestResult.FAILURE) {
-//            String screenshotLocation = BrowserUtils.getScreenshot(testResult.getName());
-//            try {
-//                extentTest.fail(testResult.getName());//test name that failed
-//                extentTest.addScreenCaptureFromPath(screenshotLocation);//screenshot as an evidence
-//                extentTest.fail(testResult.getThrowable());//error message
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                throw new RuntimeException("Failed to attach screenshot");
-//            }
-//        } else if (testResult.getStatus() == ITestResult.SUCCESS) {
-//            extentTest.pass(testResult.getName());
-//        } else if (testResult.getStatus() == ITestResult.SKIP) {
-//            extentTest.skip(testResult.getName());
-//        }
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            String screenshotLocation = BrowserUtils.getScreenshot(testResult.getName());
+            try {
+                extentTest.fail(testResult.getName());//test name that failed
+               extentTest.addScreenCaptureFromPath(screenshotLocation);//screenshot as an evidence         extentTest.fail(testResult.getThrowable());//error message
+            } catch (IOException e) {
+               e.printStackTrace();
+                throw new RuntimeException("Failed to attach screenshot");
+           }
+        } else if (testResult.getStatus() == ITestResult.SUCCESS) {
+           extentTest.pass(testResult.getName());
+        } else if (testResult.getStatus() == ITestResult.SKIP) {
+            extentTest.skip(testResult.getName());
+       }
         BrowserUtils.wait(3);
         Driver.closeDriver();
     }
